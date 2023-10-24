8d3b5d980f94d02ed27a07fc8604ae0ef9e669fa
adenord <alexandre.denord@gmail.com>
2023-10-24 05:19:10 +0200

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: adenord <alexandre.denord@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 14:53:42 by adenord           #+#    #+#             */
-/*   Updated: 2023/10/23 18:39:21 by adenord          ###   ########.fr       */
+/*   Updated: 2023/10/23 18:59:15 by adenord          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,7 @@ static char	*find_line(int fd, char **storage)
 	storage[fd] = tmp;
 	if (!ret || !tmp || ft_strlen(ret) == 0)
 	{
+		free(storage[fd]);
 		free(ret);
 		storage[fd] = NULL;
 		return (NULL);
@@ -68,7 +69,7 @@ static char	*reading_fd(int fd, char **storage, char *tmp)
 	char	*tmp2;
 
 	bytes_read = 1;
-	while (bytes_read > 0)
+	while (bytes_read >= 0)
 	{
 		ft_memset(tmp, 0, BUFFER_SIZE + 1);
 		bytes_read = read(fd, tmp, BUFFER_SIZE);
